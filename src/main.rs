03b839120091332f108a69aa36cfe2a128f7676a
jswizzy <smithj@g.cofc.edu>
2020-03-21 22:31:23 -0400

    init commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,154 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use lazy_static::lazy_static;
+use regex::Regex;
+use slab::Slab;
+use std::fmt;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+type UserId = u64;
+
+struct UserData;
+
+type UserDb = Arc<Mutex<Slab<UserData>>>;
+
+lazy_static! {
+    static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+    static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?)/?)?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
 }
+
+impl fmt::Display for UserData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("{}")
+    }
+}
+
+async fn handler(req: Request<Body>, user_db: UserDb) -> Result<Response<Body>, Error> {
+    let response = {
+        let method = req.method();
+        let path = req.uri().path();
+        if INDEX_PATH.is_match(path) {
+            if method == Method::GET {
+                Response::new(INDEX.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if USERS_PATH.is_match(path) {
+            let users = user_db.lock().unwrap();
+            if method == Method::GET {
+                let list = users
+                    .iter()
+                    .map(|(id, _)| id.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",");
+                Response::new(list.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if let Some(cap) = USER_PATH.captures(path) {
+            let mut users = user_db.lock().unwrap();
+            let user_id = cap
+                .name("user_id")
+                .and_then(|m| m.as_str().parse::<UserId>().ok().map(|x| x as usize));
+            match (method, user_id) {
+                (&Method::GET, Some(id)) => {
+                    if let Some(data) = users.get(id) {
+                        Response::new(data.to_string().into())
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                (&Method::POST, None) => {
+                    let id = users.insert(UserData);
+                    Response::new(id.to_string().into())
+                }
+                (&Method::POST, Some(_)) => response_with_code(StatusCode::BAD_REQUEST),
+                (&Method::PUT, Some(id)) => {
+                    if let Some(user) = users.get_mut(id) {
+                        *user = UserData;
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                (&Method::DELETE, Some(id)) => {
+                    if users.contains(id) {
+                        users.remove(id);
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                _ => response_with_code(StatusCode::METHOD_NOT_ALLOWED),
+            }
+        } else {
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from(NOT_FOUND))
+                .unwrap()
+        }
+    };
+
+    Ok(response)
+}
+
+fn response_with_code(status_code: StatusCode) -> Response<Body> {
+    Response::builder()
+        .status(status_code)
+        .body(Body::empty())
+        .unwrap()
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let user_db = Arc::new(Mutex::new(Slab::new()));
+
+    let make_svc = make_service_fn(move |_conn| {
+        let user_db = user_db.clone();
+
+        async move { Ok::<_, Error>(service_fn(move |req| handler(req, user_db.clone()))) }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    if let Err(e) = graceful.await {
+        eprint!("Server error: {}", e);
+    }
+}
+
+const INDEX: &str = r#"
+<!doctype html>
+<html>
+    <head>
+        <title>Rust Microservice</title>
+    </head>
+    <body>
+        <h3>Rust Microservice</h3>
+    </body>
+</html>
+"#;
+
+const NOT_FOUND: &str = r#"
+<!doctype html>
+<html>
+    <head>
+        <title>Rust Microservice</title>
+    </head>
+    <body>
+        <h3>404 Page Not Found</h3>
+    </body>
+</html>
+"#;
